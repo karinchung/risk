81d801cd84628036cb1a58b064f19fe77b2a4c54
Karin Chung <karin.chung13@gmail.com>
2017-03-31 23:49:40 -0700

    added turn switch and start game button

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-var playerOne = {name: 'playerOne', armySize: 100, tiles: 0}
-var playerTwo = {name: 'playerTwo', armySize: -100, tiles: 0}
+var risk = {
+  playerOne: {name: 'playerOne', armySize: 100, tiles: 0},
+  playerTwo: {name: 'playerTwo', armySize: -100, tiles: 0}
+}
 
 var gridAmount = 28
 var $container = $('.container')
+var $body = $('body')
 var allTiles = []
+var currentPlayer = risk.playerOne;
+var $startButton = $('<button>Start Game</button>')
+
+$body.append($startButton)
+$startButton.on('click', startGame)
 
 
 // Tile constructor
 function Tile() {
-  $container.append($("<div></div>"))
+  $container.append($('<div></div>'))
   this.armySize = 0;
   this.fortification = null;
   this.obstacle = null;
@@ -19,3 +27,20 @@ function Tile() {
 for (var i = 0; i < gridAmount; i++) {
   allTiles[i] = new Tile
 }
+
+// switch turns
+function switchTurns() {
+  if(currentPlayer == risk.playerOne) {
+    currentPlayer = risk.playerTwo
+  } else {
+    currentPlayer = risk.playerOne
+  }
+}
+
+// start game button and make armies appear
+function startGame() {
+  allTiles[0].armySize += risk.playerOne.armySize
+  allTiles[gridAmount - 1].armySize += risk.playerTwo.armySize
+}
+
+// toggles class based on armySize being + or - 0dffrfd
