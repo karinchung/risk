8cd45abcf7e8f55a6c18fa27e8f2b401cd3d9448
Karin Chung <karin.chung13@gmail.com>
2017-04-04 10:50:30 -0700

    added viable tiles only movement

--- main.js
+++ main.js
@@ -3,32 +3,29 @@ var players = {
   playerTwo: {name: null, armySize: 100, tiles: [], color: 'red'}
 };
 
-//armySize is really just starting army size unless it's zero in which case you dead
-// count tiles for win condition
-
-var gridAmount = 35
-var $container = $('.container')
-var $header = $('header')
-var $body = $('body')
-var board = []
+var gridAmount = 35;
+var $container = $('.container');
+var $body = $('body');
+var board = [];
 var currentPlayer = players.playerOne;
 var otherPlayer = players.playerTwo;
-var $startButton = $('<button>Start Game</button>')
-var clicks = 0
-var currentArmy = 0
+var $startButton = $('<button>Start Game</button>');
+var clicks = 0;
+var currentArmy = 0;
 var lastCell = null;
+var viableTiles = [];
 
 $body.append($startButton);
 $startButton.on('click', startGame);
 
 // tile creation constructor
 function Tile() {
-  this.tileDiv = ($('<div></div>', {'class': 'neutral'}));
+  this.tileDiv = ($('<div></div>', {'class': 'neutral', 'id': i}));
   this.owner = null;
   this.armySize = 0;
   this.fortification = null;
   this.obstacle = null;
-  this.showNewClass = function() {
+  this.showNewClass = function() { // refactor later and take out
     if (this.owner == players.playerOne) {
       if (this.tileDiv.hasClass('neutral')) {
         this.tileDiv.removeClass('neutral')
@@ -43,11 +40,6 @@ function Tile() {
       }
     };
     this.tileDiv.on('click', moveArmy);
-
-};
-
-function divide(armyToDivide) {
-  return Number(armyToDivide / 2)
 };
 
 // Creates all the tiles
@@ -71,15 +63,48 @@ function startGame() {
   $startButton.off('click', startGame)
 };
 
+// switch turns
+function switchTurns() {
+  if(currentPlayer == players.playerOne) {
+    currentPlayer = players.playerTwo
+    otherPlayer = players.playerOne
+
+  } else {
+    currentPlayer = players.playerOne
+    otherPlayer = players.playerTwo
+  }
+};
+
+function divide(armyToDivide) {
+  return Number(armyToDivide / 2)
+};
+
 function moveArmy() {
   // first click must be a tile color of the current player
   if(clicks == 0 && $(this).attr('class') == currentPlayer.color) {
     clicks++
     currentArmy = this.innerText
     lastCell = this
+    if (lastCell.id -1 > 0) { // can this be put into a separate function?
+      viableTiles[0] = Number(lastCell.id - 1)
+    }
+    if (lastCell.id - 7 > 0) {
+      viableTiles[1] = Number(lastCell.id - 7)
+    }
+    if (lastCell.id + 1 < gridAmount - 1) {
+      viableTiles[2] = Number(lastCell.id + 1)
+    }
+    if (lastCell.id + 7 < gridAmount - 1) {
+      viableTiles[3] = Number(lastCell.id + 7)
+    }
+    // add a function here that toggles the viableTiles css to orange
+
   }
   // second click: if a neutral tile is clicked
-  else if (clicks == 1 && $(this).attr('class') == 'neutral') {
+  else if ((clicks == 1 && $(this).attr('class') == 'neutral') &&
+  (this.id == viableTiles[0] || this.id == viableTiles[1] || this.id == viableTiles[2] || this.id == viableTiles[3]) ) {
+    // bug: you can click on yourself and delete yourself so that's great
+    console.log(this.id)
     var halfThisArmy = divide(currentArmy)
     $(this).removeClass('neutral')
     $(this).toggleClass(currentPlayer.color)
@@ -130,17 +155,5 @@ function moveArmy() {
   }
   else {
     console.log("Not a valid move")
-  }
-};
-
-// switch turns
-function switchTurns() {
-  if(currentPlayer == players.playerOne) {
-    currentPlayer = players.playerTwo
-    otherPlayer = players.playerOne
-
-  } else {
-    currentPlayer = players.playerOne
-    otherPlayer = players.playerTwo
   }
 };
